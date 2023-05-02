0630f8d10b7434c15af99a134fe6d119b55214a2
alex278fi <alexgonza278fi@gmail.com>
2023-05-01 21:02:01 -0300

    incorporacion submenu punto 3 4 5 y incoporacion contenido de historia

--- contact_process.php
+++ contact_process.php
@@ -6,7 +6,6 @@
     $subject = $_REQUEST['subject'];
     $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
-
     $headers = "From: $from";
 	$headers = "From: " . $from . "\r\n";
 	$headers .= "Reply-To: ". $from . "\r\n";

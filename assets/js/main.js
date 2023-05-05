ecff99b5f1cbd8bca144e406b1c36a7aa5709b9d
alex278fi <alexgonza278fi@gmail.com>
2023-05-04 22:28:02 -0300

    mejorar carga de pagina

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,7 @@
 
   /* 1. Proloder */
   $(window).on('load', function () {
-    $('#preloader-active').delay(450).fadeOut('slow');
+    $('#preloader-active').delay(250).fadeOut('fast');
     $('body').delay(450).css({
       'overflow': 'visible'
     });
